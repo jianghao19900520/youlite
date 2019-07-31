abfb0c363a8397d7014f2d2e2636d3b1fb61fc54
jianghao <jianghao19900520@foxmail.com>
2019-07-31 14:22:42 +0800

    updata

---
+++ app/src/main/java/cn/com/test/activity/OrderDetailActivity.java
@@ -0,0 +1,37 @@
+package cn.com.test.activity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.yanzhenjie.nohttp.RequestMethod;
+
+import butterknife.BindView;
+import cn.com.test.R;
+import cn.com.test.base.BaseActivity;
+
+public class OrderDetailActivity extends BaseActivity {
+
+    @BindView(R.id.title)
+    TextView title;
+
+    @Override
+    public void setContent(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_my_order);
+    }
+
+    @Override
+    public void initTitle() {
+        title.setText("订单订单");
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loadData(int what, String[] value, String msg, RequestMethod method) {
+
+    }
+
+}

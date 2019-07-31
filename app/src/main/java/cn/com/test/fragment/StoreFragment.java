00f9eb48d68b1ec5da46a3eff5e54c9286ace3f3
jianghao <jianghao19900520@foxmail.com>
2019-07-31 12:05:27 +0800

    updata

--- app/src/main/java/cn/com/test/fragment/StoreFragment.java
+++ app/src/main/java/cn/com/test/fragment/StoreFragment.java
@@ -34,6 +34,7 @@ import cn.com.test.R;
 import cn.com.test.activity.CartActivity;
 import cn.com.test.activity.GoodsInfoActivity;
 import cn.com.test.activity.HomeActivity;
+import cn.com.test.activity.MyOrderActivity;
 import cn.com.test.adapter.CommAdapter;
 import cn.com.test.adapter.CommViewHolder;
 import cn.com.test.base.BaseFragment;
@@ -214,11 +215,14 @@ public class StoreFragment extends BaseFragment implements OnBannerListener {
         }
     }
 
-    @OnClick({R.id.store_cart_btn})
+    @OnClick({R.id.store_cart_btn, R.id.store_my_order_btn})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.store_cart_btn:
                 startActivity(new Intent(mContext, CartActivity.class));
+                break;
+            case R.id.store_my_order_btn:
+                startActivity(new Intent(mContext, MyOrderActivity.class));
                 break;
         }
     }

6d786ad597a642f59c6c35557414a3d90676dedb
jianghao <jianghao19900520@foxmail.com>
2019-08-07 15:16:32 +0800

    updata

--- app/src/main/java/cn/com/test/fragment/MineFragment.java
+++ app/src/main/java/cn/com/test/fragment/MineFragment.java
@@ -1,38 +1,20 @@
 package cn.com.test.fragment;
 
-import android.Manifest;
+
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.Settings;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.yanzhenjie.nohttp.Headers;
-import com.yanzhenjie.nohttp.NoHttp;
 import com.yanzhenjie.nohttp.RequestMethod;
-import com.yanzhenjie.nohttp.download.DownloadListener;
-import com.yanzhenjie.nohttp.download.DownloadQueue;
-import com.yanzhenjie.nohttp.download.DownloadRequest;
 
-import java.io.File;
-
+import butterknife.OnClick;
 import cn.com.test.R;
 import cn.com.test.activity.AboutAsActivity;
 import cn.com.test.activity.HomeActivity;
 import cn.com.test.base.BaseFragment;
-import cn.com.test.utils.AppUtils;
-import cn.com.test.utils.LogUtils;
 import cn.com.test.utils.ToastUtils;
 
 public class MineFragment extends BaseFragment {
@@ -64,4 +46,15 @@ public class MineFragment extends BaseFragment {
     public void loadData(int what, String[] value, String msg, RequestMethod method) {
 
     }
+
+    @OnClick({R.id.mine_head_layout})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.mine_head_layout:
+                ToastUtils.showShort("用户资料");
+                break;
+        }
+    }
+
+
 }

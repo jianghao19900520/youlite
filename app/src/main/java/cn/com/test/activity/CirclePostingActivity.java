8063b9340bb731b2f856af9ed321e3e9f68db2e9
jianghao <jianghao19900520@foxmail.com>
2019-08-22 13:10:00 +0800

    updata

--- app/src/main/java/cn/com/test/activity/CirclePostingActivity.java
+++ app/src/main/java/cn/com/test/activity/CirclePostingActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -14,6 +15,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.View;
@@ -22,6 +24,7 @@ import android.view.Window;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -31,6 +34,7 @@ import com.yanzhenjie.nohttp.RequestMethod;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import butterknife.BindView;
@@ -38,8 +42,10 @@ import butterknife.OnClick;
 import cn.com.test.R;
 import cn.com.test.base.BaseActivity;
 import cn.com.test.utils.FileUtils;
+import cn.com.test.utils.ToastUtils;
+import pub.devrel.easypermissions.EasyPermissions;
 
-public class CirclePostingActivity extends BaseActivity {
+public class CirclePostingActivity extends BaseActivity implements EasyPermissions.PermissionCallbacks {
 
     @BindView(R.id.title)
     TextView title;
@@ -48,8 +54,9 @@ public class CirclePostingActivity extends BaseActivity {
 
     public static final int RC_TAKE_PHOTO = 1;
     public static final int RC_CHOOSE_PHOTO = 2;
-    private String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/Camera";
-    private Uri saveUri;//当前正在拍摄的照片的uri
+    private String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/Camera/";
+    private String picPath;//当前正在拍摄的照片的路径
+    private String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     @Override
     public void setContent(Bundle savedInstanceState) {
@@ -63,7 +70,6 @@ public class CirclePostingActivity extends BaseActivity {
 
     @Override
     public void init() {
-
     }
 
     @Override
@@ -87,58 +93,60 @@ public class CirclePostingActivity extends BaseActivity {
     }
 
     private void showBottomDialog() {
-        final Dialog dialog = new Dialog(this, R.style.DialogTheme);
-        View view = View.inflate(this, R.layout.dialog_custom_layout, null);
-        dialog.setContentView(view);
-        Window window = dialog.getWindow();
-        window.setGravity(Gravity.BOTTOM);
-        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialog.show();
-        dialog.findViewById(R.id.tv_take_photo).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                saveUri = Uri.fromFile(new File(path, System.currentTimeMillis() + ".jpg"));
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, saveUri);
-                startActivityForResult(intent, RC_TAKE_PHOTO);
-                dialog.dismiss();
-            }
-        });
-        dialog.findViewById(R.id.tv_take_pic).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    //未授权，申请授权(从相册选择图片需要读取存储卡的权限)
-                    ActivityCompat.requestPermissions(CirclePostingActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, RC_CHOOSE_PHOTO);
-                } else {
-                    //已授权，获取照片
+        if (EasyPermissions.hasPermissions(mContext, permissions)) {
+            //已经打开权限
+            final Dialog dialog = new Dialog(this, R.style.DialogTheme);
+            View view = View.inflate(this, R.layout.dialog_custom_layout, null);
+            dialog.setContentView(view);
+            Window window = dialog.getWindow();
+            window.setGravity(Gravity.BOTTOM);
+            window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.show();
+            dialog.findViewById(R.id.tv_take_photo).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Calendar calendar = Calendar.getInstance();  //获取当前时间，作为图标的名字
+                    int yearInt = calendar.get(Calendar.YEAR);
+                    String year = String.valueOf(yearInt);
+                    int monthInt = calendar.get(Calendar.MONTH) + 1;
+                    String month = String.valueOf(monthInt > 9 ? monthInt : ("0" + monthInt));
+                    int dayInt = calendar.get(Calendar.DAY_OF_MONTH);
+                    String day = String.valueOf(dayInt > 9 ? dayInt : ("0" + dayInt));
+                    int hourInt = calendar.get(Calendar.HOUR_OF_DAY);
+                    String hour = String.valueOf(hourInt > 9 ? hourInt : ("0" + hourInt));
+                    int minuteInt = calendar.get(Calendar.MINUTE);
+                    String minute = String.valueOf(minuteInt > 9 ? minuteInt : ("0" + minuteInt));
+                    int secondInt = calendar.get(Calendar.SECOND);
+                    String second = String.valueOf(secondInt > 9 ? secondInt : ("0" + secondInt));
+                    picPath = path + "IMG" + "_" + year + month + day + "_" + hour + minute + second + ".jpg";
+                    File picFile = new File(picPath);
+                    Uri picUri = Uri.fromFile(picFile);
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        picUri = FileProvider.getUriForFile(mContext, "cn.com.test.fileprovider", picFile);
+                        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    }
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, picUri);
+                    startActivityForResult(intent, RC_TAKE_PHOTO);
+                    dialog.dismiss();
+                }
+            });
+            dialog.findViewById(R.id.tv_take_pic).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
                     choosePhoto();
+                    dialog.dismiss();
                 }
-                dialog.dismiss();
-            }
-        });
-        dialog.findViewById(R.id.tv_cancel).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
-    }
-
-    /**
-     * 权限申请结果回调
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case RC_TAKE_PHOTO:   //拍照权限申请返回
-                if (grantResults.length == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-//                    takePhoto();
+            });
+            dialog.findViewById(R.id.tv_cancel).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
                 }
-                break;
-            case RC_CHOOSE_PHOTO:   //相册选择照片权限申请返回
-                choosePhoto();
-                break;
+            });
+        } else {
+            //没有打开相关权限、申请权限
+            EasyPermissions.requestPermissions(this, "需要获取您的相册、照相使用权限", 1, permissions);
         }
     }
 
@@ -184,9 +192,9 @@ public class CirclePostingActivity extends BaseActivity {
             case RC_TAKE_PHOTO:
                 ImageView imageView = new ImageView(mContext);
                 RequestOptions requestOptions = new RequestOptions().skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE);
-                Glide.with(mContext).load(FileUtils.getFilePathByUri(this, saveUri)).apply(requestOptions).into(imageView);
+                Glide.with(mContext).load(picPath).apply(requestOptions).into(imageView);
                 circle_posting_img_layout.addView(imageView);
-                imageView.setTag(R.id.indexTag, FileUtils.getFilePathByUri(this, saveUri));
+                imageView.setTag(R.id.indexTag, picPath);
                 imageView.setOnLongClickListener(new View.OnLongClickListener() {
                     @Override
                     public boolean onLongClick(final View view) {
@@ -202,8 +210,29 @@ public class CirclePostingActivity extends BaseActivity {
                         return true;
                     }
                 });
+                // 通知图库更新
+                sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + picPath)));
                 break;
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //框架要求必须这么写
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    //成功打开权限
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    //用户未同意权限
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        ToastUtils.showShort("1111111111111");
     }
 }
 

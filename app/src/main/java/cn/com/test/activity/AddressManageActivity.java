089543307154d6006c7fc8fa808303c7ff8c6f5d
jianghao <jianghao19900520@foxmail.com>
2019-08-27 14:41:22 +0800

    updata

---
+++ app/src/main/java/cn/com/test/activity/AddressManageActivity.java
@@ -0,0 +1,33 @@
+package cn.com.test.activity;
+
+import android.os.Bundle;
+
+import com.yanzhenjie.nohttp.RequestMethod;
+
+import cn.com.test.R;
+import cn.com.test.base.BaseActivity;
+
+public class AddressManageActivity extends BaseActivity {
+
+
+    @Override
+    public void setContent(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_address_manage);
+    }
+
+    @Override
+    public void initTitle() {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loadData(int what, String[] value, String msg, RequestMethod method) {
+
+    }
+
+}

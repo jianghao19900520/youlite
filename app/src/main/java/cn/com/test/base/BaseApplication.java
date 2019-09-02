c1cfd416f7640760d9c2a22d6b2c52a8b37d2f74
jianghao <jianghao19900520@foxmail.com>
2019-09-02 15:23:14 +0800

    update

--- app/src/main/java/cn/com/test/base/BaseApplication.java
+++ app/src/main/java/cn/com/test/base/BaseApplication.java
@@ -50,7 +50,7 @@ public class BaseApplication extends Application{
             @SuppressLint("ResourceType")
             @Override
             public RefreshHeader createRefreshHeader(Context context, RefreshLayout layout) {
-                layout.setPrimaryColorsId(R.color.backgroundColor, R.color.colorPrimary);//全局设置主题颜色
+                layout.setPrimaryColorsId(R.color.backgroundColor, R.color.color_333333);//全局设置主题颜色
                 return new ClassicsHeader(context).setTimeFormat(new DynamicTimeFormat("更新于 %s")).setSpinnerStyle(SpinnerStyle.Translate);//指定为经典Header，默认是 贝塞尔雷达Header
             }
         });

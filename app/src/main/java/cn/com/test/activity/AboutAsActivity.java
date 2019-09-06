18fede8c4f393fb7e2c8bced21a785ae67c3fc60
jianghao <jianghao19900520@foxmail.com>
2019-09-06 12:32:43 +0800

    update

--- app/src/main/java/cn/com/test/activity/AboutAsActivity.java
+++ app/src/main/java/cn/com/test/activity/AboutAsActivity.java
@@ -1,7 +1,10 @@
 package cn.com.test.activity;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -18,6 +21,8 @@ import com.yanzhenjie.nohttp.download.DownloadListener;
 import com.yanzhenjie.nohttp.download.DownloadQueue;
 import com.yanzhenjie.nohttp.download.DownloadRequest;
 
+import org.litepal.util.LogUtil;
+
 import java.io.File;
 
 import butterknife.BindView;
@@ -30,11 +35,8 @@ public class AboutAsActivity extends BaseActivity {
 
     @BindView(R.id.title)
     TextView title;
-
-    private DownloadRequest downloadRequest;
-    private String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/.youlite";
-    private String filename = "youlite.apk";
-    private ProgressDialog dialog;
+    @BindView(R.id.about_as_version_text)
+    TextView about_as_version_text;
 
     @Override
     public void setContent(Bundle savedInstanceState) {
@@ -48,7 +50,7 @@ public class AboutAsActivity extends BaseActivity {
 
     @Override
     public void init() {
-
+        about_as_version_text.setText("当前软件版本：" + getLocalVersionName(mContext));
     }
 
     @Override
@@ -57,91 +59,37 @@ public class AboutAsActivity extends BaseActivity {
     }
 
     /**
-     * 检查权限
+     * 获取本地软件版本号
      */
-    private void checkPermission() {
-        if (Build.VERSION.SDK_INT >= 26 && !mContext.getPackageManager().canRequestPackageInstalls()) {
-            //8.0及以上系统，且没有获取未知来源权限，需要先申请
-            ToastUtils.showLong("请开启安装权限");
-            Intent intent = new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES);
-            startActivityForResult(intent, 0);
-        } else {
-            installAPK();
+    public int getLocalVersion(Context ctx) {
+        int localVersion = 0;
+        try {
+            PackageInfo packageInfo = ctx.getApplicationContext()
+                    .getPackageManager()
+                    .getPackageInfo(ctx.getPackageName(), 0);
+            localVersion = packageInfo.versionCode;
+            LogUtil.d("TAG", "本软件的版本号。。" + localVersion);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
         }
+        return localVersion;
     }
 
-
-    private void installAPK() {
-        File apkFile = new File(path + "/" + filename);
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            Uri contentUri = FileProvider.getUriForFile(mContext, "cn.com.test.fileprovider", apkFile);
-            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
-        } else {
-            intent.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive");
+    /**
+     * 获取本地软件版本号名称
+     */
+    public String getLocalVersionName(Context ctx) {
+        String localVersion = "";
+        try {
+            PackageInfo packageInfo = ctx.getApplicationContext()
+                    .getPackageManager()
+                    .getPackageInfo(ctx.getPackageName(), 0);
+            localVersion = packageInfo.versionName;
+            LogUtil.d("TAG", "本软件的版本号。。" + localVersion);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
         }
-        startActivity(intent);
-    }
-
-    @OnClick({R.id.about_as_check_updata_btn})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.about_as_check_updata_btn:
-                download();
-                break;
-        }
-    }
-
-    private void download() {
-        downloadRequest = NoHttp.createDownloadRequest("http://106.52.216.72/web/youlite.apk", path, filename, false, true);
-        DownloadQueue downloadQueue = new DownloadQueue(1);
-        downloadQueue.add(0, downloadRequest, new DownloadListener() {
-            @Override
-            public void onDownloadError(int what, Exception exception) {
-                if (dialog != null) {
-                    dialog.dismiss();
-                    dialog = null;
-                }
-                ToastUtils.showLong("下载失败，请稍后重试");
-            }
-
-            @Override
-            public void onStart(int what, boolean isResume, long rangeSize, Headers responseHeaders, long allCount) {
-                if (dialog == null) {
-                    dialog = ProgressDialog.show(mContext, "提示", "下载中…", true, false, null);
-                } else {
-                    if (!dialog.isShowing()) {
-                        dialog.show();
-                    }
-                }
-            }
-
-            @Override
-            public void onProgress(int what, int progress, long fileCount, long speed) {
-                dialog.setMessage("下载" + progress + "%…");
-            }
-
-            @Override
-            public void onFinish(int what, String filePath) {
-                if (dialog != null) {
-                    dialog.dismiss();
-                    dialog = null;
-                }
-                checkPermission();
-            }
-
-            @Override
-            public void onCancel(int what) {
-                if (dialog != null) {
-                    dialog.dismiss();
-                    dialog = null;
-                }
-                ToastUtils.showLong("下载取消");
-            }
-        });
-        downloadQueue.start();
+        return localVersion;
     }
 
 }
--- app/src/main/java/cn/com/test/activity/SettingActivity.java
+++ app/src/main/java/cn/com/test/activity/SettingActivity.java
@@ -1,15 +1,28 @@
 package cn.com.test.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.v4.content.FileProvider;
 import android.text.TextUtils;
 import android.view.View;
 
+import com.yanzhenjie.nohttp.Headers;
+import com.yanzhenjie.nohttp.NoHttp;
 import com.yanzhenjie.nohttp.RequestMethod;
+import com.yanzhenjie.nohttp.download.DownloadListener;
+import com.yanzhenjie.nohttp.download.DownloadQueue;
+import com.yanzhenjie.nohttp.download.DownloadRequest;
 import com.yanzhenjie.nohttp.rest.Response;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.File;
 
 import butterknife.OnClick;
 import cn.com.test.R;
@@ -23,6 +36,10 @@ import cn.com.test.utils.ToastUtils;
 
 public class SettingActivity extends BaseActivity {
 
+    private DownloadRequest downloadRequest;
+    private String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/.youlite";
+    private String filename = "youlite.apk";
+    private ProgressDialog dialog;
 
     @Override
     public void setContent(Bundle savedInstanceState) {
@@ -88,7 +105,7 @@ public class SettingActivity extends BaseActivity {
                 }
                 break;
             case R.id.setting_check_update_layout:
-                startActivity(new Intent(mContext, AboutAsActivity.class));
+                download();
                 break;
             case R.id.setting_logout_layout:
                 if (LoginUtils.getInstance().isLogin()) {
@@ -99,4 +116,83 @@ public class SettingActivity extends BaseActivity {
                 break;
         }
     }
+
+    private void download() {
+        downloadRequest = NoHttp.createDownloadRequest("http://106.52.216.72/web/youlite.apk", path, filename, false, true);
+        DownloadQueue downloadQueue = new DownloadQueue(1);
+        downloadQueue.add(0, downloadRequest, new DownloadListener() {
+            @Override
+            public void onDownloadError(int what, Exception exception) {
+                if (dialog != null) {
+                    dialog.dismiss();
+                    dialog = null;
+                }
+                ToastUtils.showLong("下载失败，请稍后重试");
+            }
+
+            @Override
+            public void onStart(int what, boolean isResume, long rangeSize, Headers responseHeaders, long allCount) {
+                if (dialog == null) {
+                    dialog = ProgressDialog.show(mContext, "提示", "下载中…", true, false, null);
+                } else {
+                    if (!dialog.isShowing()) {
+                        dialog.show();
+                    }
+                }
+            }
+
+            @Override
+            public void onProgress(int what, int progress, long fileCount, long speed) {
+                dialog.setMessage("下载" + progress + "%…");
+            }
+
+            @Override
+            public void onFinish(int what, String filePath) {
+                if (dialog != null) {
+                    dialog.dismiss();
+                    dialog = null;
+                }
+                checkPermission();
+            }
+
+            @Override
+            public void onCancel(int what) {
+                if (dialog != null) {
+                    dialog.dismiss();
+                    dialog = null;
+                }
+                ToastUtils.showLong("下载取消");
+            }
+        });
+        downloadQueue.start();
+    }
+
+    /**
+     * 检查权限
+     */
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= 26 && !mContext.getPackageManager().canRequestPackageInstalls()) {
+            //8.0及以上系统，且没有获取未知来源权限，需要先申请
+            ToastUtils.showLong("请开启安装权限");
+            Intent intent = new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES);
+            startActivityForResult(intent, 0);
+        } else {
+            installAPK();
+        }
+    }
+
+    private void installAPK() {
+        File apkFile = new File(path + "/" + filename);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            Uri contentUri = FileProvider.getUriForFile(mContext, "cn.com.test.fileprovider", apkFile);
+            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+        } else {
+            intent.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive");
+        }
+        startActivity(intent);
+    }
+
 }

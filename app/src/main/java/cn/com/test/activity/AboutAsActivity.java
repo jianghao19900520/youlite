a883a1239d05604efcfdace27209b707789e3ac4
jianghao <jianghao19900520@foxmail.com>
2019-09-06 13:37:21 +0800

    update

--- app/src/main/java/cn/com/test/activity/AboutAsActivity.java
+++ app/src/main/java/cn/com/test/activity/AboutAsActivity.java
@@ -1,35 +1,16 @@
 package cn.com.test.activity;
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.Intent;
+
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.Settings;
-import android.support.v4.content.FileProvider;
-import android.view.View;
 import android.widget.TextView;
 
-import com.yanzhenjie.nohttp.Headers;
-import com.yanzhenjie.nohttp.NoHttp;
 import com.yanzhenjie.nohttp.RequestMethod;
-import com.yanzhenjie.nohttp.download.DownloadListener;
-import com.yanzhenjie.nohttp.download.DownloadQueue;
-import com.yanzhenjie.nohttp.download.DownloadRequest;
-
-import org.litepal.util.LogUtil;
-
-import java.io.File;
 
 import butterknife.BindView;
-import butterknife.OnClick;
 import cn.com.test.R;
 import cn.com.test.base.BaseActivity;
-import cn.com.test.utils.ToastUtils;
 
 public class AboutAsActivity extends BaseActivity {
 
@@ -50,46 +31,19 @@ public class AboutAsActivity extends BaseActivity {
 
     @Override
     public void init() {
-        about_as_version_text.setText("当前软件版本：" + getLocalVersionName(mContext));
+        try {
+            PackageInfo packageInfo = getApplicationContext()
+                    .getPackageManager()
+                    .getPackageInfo(getPackageName(), 0);
+            about_as_version_text.setText("当前软件版本：" + packageInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void loadData(int what, String[] value, String msg, RequestMethod method) {
 
-    }
-
-    /**
-     * 获取本地软件版本号
-     */
-    public int getLocalVersion(Context ctx) {
-        int localVersion = 0;
-        try {
-            PackageInfo packageInfo = ctx.getApplicationContext()
-                    .getPackageManager()
-                    .getPackageInfo(ctx.getPackageName(), 0);
-            localVersion = packageInfo.versionCode;
-            LogUtil.d("TAG", "本软件的版本号。。" + localVersion);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        return localVersion;
-    }
-
-    /**
-     * 获取本地软件版本号名称
-     */
-    public String getLocalVersionName(Context ctx) {
-        String localVersion = "";
-        try {
-            PackageInfo packageInfo = ctx.getApplicationContext()
-                    .getPackageManager()
-                    .getPackageInfo(ctx.getPackageName(), 0);
-            localVersion = packageInfo.versionName;
-            LogUtil.d("TAG", "本软件的版本号。。" + localVersion);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        return localVersion;
     }
 
 }
